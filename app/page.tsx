2ca6eb798cc8aec94230889bd3a361c594a62d79
J Scott Smith <jscsmith@gmail.com>
2025-05-18 14:41:44 -0700

    chore: update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Gush } from "./components/gush";
 import { Hero } from "./components/hero";
 import { SiteWrapper } from "./components/site-wrapper";
 import { content } from "./content";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
@@ -11,13 +12,39 @@ export default function Home() {
         <Placeholder className="absolute inset-0" />
         {/* <Gush /> */}
       </Hero>
-      <SiteWrapper className="w-full min-h-screen">
-        <section className="w-full gap-lg">
-          <h1 className="text-6xl">{content.home.headline}</h1>
-          <h2 className="text-2xl">{content.home.subhead}</h2>
-          <p>{content.home.copy}</p>
+      <SiteWrapper
+        className="w-full min-h-screen relative flex flex-col my-lg gap-lg"
+        maxWidth
+      >
+        <Card>
+          <section className="h-[70vh] w-full gap-lg p-xl grid items-center">
+            <h1 className="text-8xl font-futura font-bold italic text-balance">
+              {content.home.headline}
+            </h1>
+          </section>
+        </Card>
+
+        <section className="h-[70vh] w-full gap-lg p-xl grid md:grid-cols-2 items-center">
+          <div>
+            <h2 className="text-5xl text-balance">{content.home.subhead}</h2>
+          </div>
+
+          <Placeholder className="h-full" />
         </section>
       </SiteWrapper>
+
+      <section className="">
+        <Placeholder>
+          <SiteWrapper
+            className="w-full min-h-screen relative flex items-center justify-center"
+            maxWidth
+          >
+            <p className="text-4xl my-6xl text-balance text-center max-w-screen-lg">
+              {content.home.copy}
+            </p>
+          </SiteWrapper>
+        </Placeholder>
+      </section>
       {/* <h1 className="font-futura font-black uppercase text-[30vmin]">Gush</h1> */}
     </main>
   );

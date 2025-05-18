43ec3ee83cc00a1b29d162d83fe83659aea41b8f
J Scott Smith <jscsmith@gmail.com>
2025-05-18 14:40:53 -0700

    chore: add max width to site wrapper

--- app/components/site-wrapper.tsx
+++ app/components/site-wrapper.tsx
@@ -8,12 +8,14 @@ type SiteWrapperProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>> & {
   padY?: boolean;
   padT?: boolean;
   padB?: boolean;
+  maxWidth?: boolean;
 };
 
 export function SiteWrapper({
   padY,
   padT,
   padB,
+  maxWidth,
   className,
   ...props
 }: SiteWrapperProps) {
@@ -22,6 +24,7 @@ export function SiteWrapper({
       className={clsx(
         className,
         "px-base",
+        maxWidth && "max-w-screen-xl mx-auto",
         padY && "py-base",
         padT && "pt-base",
         padB && "pb-base"
